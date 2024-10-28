f49e9c9a67309c53fd1333527125a168d2accbe1
Amit Kaminsky <amitkamin131@gmail.com>
2024-10-28 22:01:51 +0200

    Update __main__.py
    Changed to default settings

--- __main__.py
+++ __main__.py
@@ -4,7 +4,7 @@ from game import Game, set_game_settings
 ######################################################
 # Change game settings:
 SCALE_FACTOR = 0.75 # 0.4 <= SCALE_FACTOR < 1. Recommended: 0.75
-asteriod_amount = 2
+asteriod_amount = 6
 bullets_amount = 3
 powered_up_bullets = 5
 ######################################################
